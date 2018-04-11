60b2b9cccf733341bba7d73a7764ab5449ab71a9
Sunny Lam <sunny.lam@gmail.com>
2018-04-11 09:29:39 -0400

    revised code

--- super_stopwatch/super_stopwatch_041118_1.py
+++ super_stopwatch/super_stopwatch_041118_1.py
@@ -24,3 +24,16 @@ startTime = time.time() # get the first lap's start time
 lastTime = startTime
 lapNum = 1
 
+# start tracking lap times
+try:
+	while True:
+		input()
+		lapTime = round( time.time() - lastTime, 2 )
+		totalTime = round( time.time() - startTime, 2 )
+		print( 'Lap #%s: %s (%s)' % (lapNum,totalTime,lapTime), end=' ' )
+		lapNum +=1
+		lastTime = time.time() # reset the last lap time
+except KeyboardInterrupt:
+	# handle the Ctrl-C exception to keep its error message from displaying
+	print('\nDone.')
+
