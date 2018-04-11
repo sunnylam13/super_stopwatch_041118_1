8150ccffcc1a42f3930211c53394734aa792b2ef
Sunny Lam <sunny.lam@gmail.com>
2018-04-11 09:25:30 -0400

    revised code

--- super_stopwatch/super_stopwatch_041118_1.py
+++ super_stopwatch/super_stopwatch_041118_1.py
@@ -5,3 +5,22 @@
 # USAGE
 # python3 super_stopwatch_041118_1.py
 
+import time
+
+import logging
+logging.basicConfig(level=logging.DEBUG, format=" %(asctime)s - %(levelname)s - %(message)s")
+# logging.disable(logging.CRITICAL)
+
+# show program instructions
+print("""
+	Press ENTER to start.  After, press ENTER to 'click' the stopwatch.\n
+	Press Ctrl-C to quit.
+	""")
+
+input() # press Enter to begin
+print('Started')
+
+startTime = time.time() # get the first lap's start time
+lastTime = startTime
+lapNum = 1
+
